6849a08d8decf36e9657d63c19c6bfb439f2b4d9
skydreamweb <info@skydream.info>
2020-07-25 13:46:56 +0200

    Register sccreen

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {}
+  actions: {
+    register(payload){ // payload = data recived from RegisterUser.vue
+      return axios.post('//localhost:3000/register', payload) // sending payload to server
+      .then(response => {
+       console.log("User data is", response.data);
+      })
+    }
+  }
 })
